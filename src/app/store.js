19199bc4017d6d74bc18b4f4a7a5e81f27704bcb
od-c0d3r <omar_rashad@aol.com>
2022-01-17 23:24:01 +0200

    Repo: move the store to app folder

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './books/booksSlice';
+import booksReducer from '../features/books/booksSlice';
 
 export const store = configureStore({
   reducer: {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
-import { store } from './features/store';
+import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 
 
 
