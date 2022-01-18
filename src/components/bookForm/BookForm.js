26ee46ce2181f5098910a7247f8e55bb07966a12
od-c0d3r <omar_rashad@aol.com>
2022-01-18 11:51:34 +0200

    Feat: apply add new book functionality

---
+++ src/components/bookForm/BookForm.js
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../features/books/booksReducer';
+
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const sendNewBookAction = (e) => {
+    dispatch(addBook(
+      {
+        title: e.target[0].value,
+        author: e.target[1].value,
+      },
+    ));
+
+    e.target.reset();
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={sendNewBookAction}>
+      <input id="inputBookTitle" type="text" placeholder="Enter Book Title" required />
+      <input id="inputBookAuthor" type="text" placeholder="Author Name" required />
+      <input type="submit" value="Add new book" />
+    </form>
+  );
+};
+
+export default BookForm;
--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import BookCard from '../bookCard/BookCard';
+import BookForm from '../bookForm/BookForm';
 
 const Books = () => {
   const books = useSelector((state) => state.booksState.books);
   const booksItem = books.map((book) => <BookCard key={book.title} data={book} />);
-  return booksItem;
+  return (
+    <>
+      <BookForm />
+      {booksItem}
+    </>
+  );
 };
 
 export default Books;
