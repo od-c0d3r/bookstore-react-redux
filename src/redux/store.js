0c512342cb64cb3f988dc236dbbf3c4ee0340884
od-c0d3r <omar_rashad@aol.com>
2022-01-19 12:05:05 +0200

    Install: redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksState from './books/booksReducer';
 
 const reducers = combineReducers({
@@ -8,7 +9,7 @@ const reducers = combineReducers({
 
 const store = createStore(
   reducers,
-  applyMiddleware(logger),
+  applyMiddleware(logger, thunk),
 );
 
 export default store;
