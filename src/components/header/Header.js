78056824eddaaf5f3a212601cb2e47357623827d
od-c0d3r <omar_rashad@aol.com>
2022-01-20 17:21:22 +0200

    Css: apply header styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,17 @@
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
+import face from '../../face.png';
 
 const Header = () => (
   <div id="navHeader">
-    <NavLink activeclassname="active" to="home">BOOKS</NavLink>
-    <NavLink activeclassname="active" to="categories">CATEGORIES</NavLink>
+    <div className="flexItem">
+      <span>Bookstore CMS</span>
+      <NavLink activeclassname="active" to="home">BOOKS</NavLink>
+      <NavLink activeclassname="active" to="categories">CATEGORIES</NavLink>
+    </div>
+    <div className="profileIcon">
+      <img className="profileIconImg" src={face} alt="profile-icon" />
+    </div>
   </div>
 );
 
